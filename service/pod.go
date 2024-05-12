9f8704212ac37aa08c4d3915b508bf178360879d
alan <sz9751210@gmail.com>
2024-05-12 14:47:03 +0800

    add get pod service handler

--- service/pod.go
+++ service/pod.go
@@ -1,12 +1,55 @@
 package service
 
-import corev1 "k8s.io/api/core/v1"
+import (
+	"context"
+	"errors"
+
+	"github.com/wonderivan/logger"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
 
 // Pod 是全局可變的 pod 實例。
 var Pod pod
 
 // pod 結構體是空的，可能是因為實際的實現細節在其他地方或尚未實現。
 type pod struct {
+}
+
+type PodsResp struct {
+	Total int          `json:"total"`
+	Items []corev1.Pod `json:"items"`
+}
+
+func (p *pod) GetPods(filterName, namespace string, limit, page int) (podsResp *PodsResp, err error) {
+	podList, err := K8s.ClientSet.CoreV1().Pods(namespace).List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		logger.Info("get podList error: ", err.Error())
+		return nil, errors.New("get podList error" + err.Error())
+	}
+	selectableData := &dataSelector{
+		GenericDataList: p.toCells(podList.Items),
+		DataSelect: &DataSelectQuery{
+			Filter: &FilterQuery{
+				Name: filterName,
+			},
+			Paginate: &PaginateQuery{
+				Limit: limit,
+				Page:  page,
+			},
+		},
+	}
+	filtered := selectableData.Filter()
+	total := len(filtered.GenericDataList)
+
+	data := filtered.Sort().Paginate()
+	pods := p.fromCells(data.GenericDataList)
+	podsResp = &PodsResp{
+		Total: total,
+		Items: pods,
+	}
+	return podsResp, nil
+
 }
 
 // toCells 方法將 corev1.Pod 的切片轉換成 DataCell 的切片。
