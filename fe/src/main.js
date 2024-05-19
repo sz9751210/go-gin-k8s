64fb4467b1ebc07a815910dd6388a4d61cf78e12
alan <sz9751210@gmail.com>
2024-05-19 18:09:18 +0800

    init main.js

--- fe/src/main.js
+++ fe/src/main.js
@@ -1,4 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import * as ELIcons from '@element-plus/icons-vue'
+import router from './router'
+
+const app = createApp(App)
+for (const iconName in ELIcons) {
+    app.component(iconName, ELIcons[iconName])
+}
+app.use(ElementPlus)
+app.use(router)
+app.mount('#app')
+ 
