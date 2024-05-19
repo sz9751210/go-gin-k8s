ccc86d53f3fbe2586c4bc7a603f14df2e821b9ea
alan <sz9751210@gmail.com>
2024-05-19 16:38:44 +0800

    setting vue config

--- fe/vue.config.js
+++ fe/vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  devServer: {
+    host: '0.0.0.0',
+    port: 7070,
+    open: true // 啟動後自動打開頁面
+  },
+  transpileDependencies: true, // 忽略警告
+  lintOnSave: false, // 禁用eslint, 關閉語法檢查
 })
