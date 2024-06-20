db19c120aaf6ceb8ac617d91eb672c5630732ec1
alan_wang <alan_wang@net-ease.com.tw>
2024-06-20 17:21:34 +0800

    add cors config

--- be/go.mod
+++ be/go.mod
@@ -5,6 +5,7 @@ go 1.22.0
 toolchain go1.22.3
 
 require (
+	github.com/gin-contrib/cors v1.7.2
 	github.com/gin-gonic/gin v1.10.0
 	github.com/wonderivan/logger v1.0.0
 	k8s.io/api v0.30.0
--- be/main.go
+++ be/main.go
@@ -5,6 +5,7 @@ import (
 	"k8s-go-gin/controller"
 	"k8s-go-gin/service"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,6 +14,15 @@ func main() {
 	service.K8s.Init()
 
 	r := gin.Default()
+
+    r.Use(cors.New(cors.Config{
+        AllowOrigins:     []string{"http://localhost:7070"},
+        AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+        AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+        ExposeHeaders:    []string{"Content-Length"},
+        AllowCredentials: true,
+    }))
+
 	controller.Router.Init(r)
 	r.Run(config.ListerAddr)
 }
