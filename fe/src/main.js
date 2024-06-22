cc2c7e663a421b565225d42a95966570500de6eb
alan <sz9751210@gmail.com>
2024-06-22 13:33:24 +0800

    import code-mirror editor

--- fe/src/main.js
+++ fe/src/main.js
@@ -1,16 +1,22 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-import * as ELIcons from '@element-plus/icons-vue'
-import router from './router'
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import * as ELIcons from "@element-plus/icons-vue";
+import router from "./router";
 
-const app = createApp(App)
+// codemirror 編輯器
+import { GlobalCmComponent } from "codemirror-editor-vue3";
+// 引入主題 可以從 codemirror/theme/ 下引入多個
+import "codemirror/theme/idea.css";
+// 引入語言模式 可以從 codemirror/mode/ 下引入多個
+import "codemirror/mode/yaml/yaml.js";
+
+const app = createApp(App);
 for (const iconName in ELIcons) {
-    app.component(iconName, ELIcons[iconName])
+  app.component(iconName, ELIcons[iconName]);
 }
-app.use(ElementPlus)
-app.use(router)
-app.mount('#app')
- 
+app.use(ElementPlus);
+app.use(router);
+app.mount("#app");
--- fe/src/views/common/Config.js
+++ fe/src/views/common/Config.js
@@ -1,4 +1,25 @@
 export default {
     k8sNamespaceList: 'http://localhost:9090/api/k8s/namespaces',
     k8sDeploymentCreate: 'http://localhost:9090/api/k8s/deployment/create',
+
+    // 編輯器設定
+    cmOption:{
+        // 語言及語法模式
+        mode: "text/yaml",
+        // 主題
+        theme: "idea",
+        // 顯示行數
+        lineNumbers: true,
+        // 智能縮進
+        smartIndent: true,
+        // 智能縮進長度
+        smartIndent: 4,
+        // 顯示選中行的樣式
+        styleActiveLine: true,
+        // 當光標位於匹配的方括號旁邊時，都會高亮顯示
+        matchBrackets: true,
+        readOnly: false,
+        // 自動換行
+        lineWrapping: true
+    }
 }
